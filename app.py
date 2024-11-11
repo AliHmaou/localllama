3fab7771137a564975fe3562f15feed1c2512346
alihmaou <ali.hmaou@gmail.com>
2024-11-11 01:28:17 +0100

    Selection d'un modele depuis la megaliste

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 import gradio as gr
 
 # Configuration des logs
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler(sys.stdout)] )
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler(sys.stdout)] )
 logger = logging.getLogger(__name__)
 
 def getModelInstance(repo_id="bartowski/gemma-2-2b-it-GGUF", filename="gemma-2-2b-it-Q5_K_S.gguf",
@@ -63,6 +63,7 @@ def getModelChatResponse( message, messages_history):
 
 
 import duckdb as ddb
+# You can see and choose your model from the trending GGUF list dataset :  
 models_list = ddb.sql(
     """
 	SELECT model_rank,repo_id, default_file_name, repo_url
@@ -72,8 +73,12 @@ models_list = ddb.sql(
     order by model_rank;"""
 ).to_df()
 
-repo_id=models_list[16]["repo_id"]
-filename=models_list[16]["filename"]
+# Filtrer pour model_rank = 17
+filtered_model = models_list[models_list["model_rank"] == 17]
+repo_id = filtered_model.iloc[0]["repo_id"]
+filename = filtered_model.iloc[0]["default_file_name"]
+
+logger.info(filtered_model)
 
 n_ctx=4048
 verbose=False
@@ -85,7 +90,7 @@ messages_history = [{"role": "user","content": "### SYSTEM : Tu es un professeur
 messages_history.append({"role": "assistant","content": "SET UP AND READY"})
 messages_history.append({"role": "user","content": "Apprend moi le mot 'manger' et ses conjugaisons"})
 
-llm, filenameloaded = getModelInstance(repo_id,filename,n_ctx,n_gpu_layers,verbose)
+llm, filenameloaded = getModelInstance(repo_id,filename,n_gpu_layers,n_ctx,verbose)
 logger.info("LLM chargé : "+filenameloaded)
 #response = getModelChatResponse("hello", messages_history, llm, stream)
 
